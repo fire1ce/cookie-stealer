ff8c2fab0df1f99dee4da06bc284bba1bad15980
Stas Kosatuhin <fire1ce2@gmail.com>
2018-03-28 14:45:24 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -36,9 +36,6 @@ if ($_POST['logintext']==$LOGIN && $_POST['passwdtext']==$PASSWD) {  ?>
 Cookies:</p>
 <table width="726" border="1">
 <tr><td>IP:</td><td>Host:</td><td>User-Agent:</td><td>Date:</td><td>Time:</td><td>Referer:</td><td>Cookie:</td></tr>
-<tr><td>77.138.65.173</td><td>77.138.65.173</td><td>Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0</td><td>21/03/2018</td><td>21:40:15</td><td>http://moodle-test.tau.ac.il/my/</td><td>_dy_ses_load_seq=10866%3A1521644932650; _dy_c_exps=; _dy_soct=31921.38566.1521644932; MoodleSession=j1efjsibmf19srdqq0l03ie3s6; _gat_atkit=1; _ga=GA1.3.864854784.1521624329; _gid=GA1.3.1115469121.1521624329</td></tr>
-<tr><td>194.90.7.17</td><td>194.90.7.17</td><td>Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0</td><td>22/03/2018</td><td>07:27:06</td><td>http://moodle-test.tau.ac.il/my/</td><td>_ga=GA1.3.864854784.1521624329; _gid=GA1.3.1115469121.1521624329; _dy_ses_load_seq=10866%3A1521644932650; _dy_c_exps=; _dy_soct=31921.38566.1521644932; MoodleSession=denseist9kpu2umpks2v752id7; _gat_atkit=1</td></tr>
-<tr><td>194.90.7.17</td><td>194.90.7.17</td><td>Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0</td><td>22/03/2018</td><td>07:28:16</td><td>Unspecified</td><td>_ga=GA1.3.864854784.1521624329; _gid=GA1.3.1115469121.1521624329; _dy_ses_load_seq=10866%3A1521644932650; _dy_c_exps=; _dy_soct=31921.38566.1521644932; MoodleSession=denseist9kpu2umpks2v752id7; _gat_atkit=1</td></tr>
 <!-- Breakpoint -->
 </table>
 <p>&nbsp;</p>
